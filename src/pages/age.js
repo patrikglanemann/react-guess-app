41dbbc59ec697d2cac428c542d86d2cc0c5cc254
PatrikG <Patrik0Gn@gmail.com>
2021-07-22 11:24:37 +0200

    Add simple click handler to fetch data

--- src/pages/age.js
+++ src/pages/age.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Age() {
+  const [list, setList] = useState();
+
+  function handleClick() {
+    const url = "https://api.agify.io?name=patrik";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setList(data.age);
+      });
+  }
+
   return (
     <div>
       <input />
-      <button>GO</button>
-      <p></p>
+      <button onClick={handleClick}>GO</button>
+      <p>{list}</p>
     </div>
   );
 }
